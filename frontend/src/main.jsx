65a549251b27ea0779b63330ee3635c8349ffe9a
Milan Javiya <milanjaviya971@gmail.com>
2026-07-25 20:53:43 +0530

    chore: bump service worker cache key to clear cached login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Cache invalidation bump
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
